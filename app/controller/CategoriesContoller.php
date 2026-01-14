251abe6fce1ce7ce12bd391376b90223c8a39ea0
lakroune <lakroune00@gmail.com>
2026-01-15 00:55:48 +0100

    refactor: remove CategorieContoler for streamlined category management; update form actions in admin_categories view for improved routing consistency

---
+++ app/controller/CategoriesContoller.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\controler;
+
+use app\model\Categorie;
+
+class CategoriesContoller
+{
+    private Categorie $categorie;
+    function __construct()
+    {
+        $this->categorie = new Categorie();
+    }
+
+    public function index()
+    {
+        if (!$this->isConnected()) {
+            header('Location: ' . PATH_ROOT);
+            exit();
+        }
+        header('Location: ' . PATH_ROOT . '/dashboard/categories');
+    }
+
+    private function isConnected(): bool
+    {
+        $connect = true;
+        if (!isset($_SESSION['Utilisateur']) or  $_SESSION['Utilisateur']->getRole() !== 'admin') {
+            $connect =  false;
+        }
+        return $connect;
+    }
+
+    public function add()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $this->remplerObject($this->categorie, $_POST);
+            $path = $this->categorie->ajouterCategorie() ? "success" : "failed";
+            header("Location: " . PATH_ROOT . "/dashboard/categories/add/$path");
+            exit;
+        }
+    }
+
+    public function delete()
+    {
+        if (isset($_POST['idCategorie'])) {
+            $path = $this->categorie->supprimerCategorie((int)$_POST['idCategorie']) ? "success" : "failed";
+            header("Location: " . PATH_ROOT . "/dashboard/categories/delete/$path");
+            exit;
+        }
+    }
+
+    private function remplerObject($object, $data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($object, $method)) {
+                $object->$method($value);
+            }
+        }
+    }
+}
